65c64d0bd23dd3d95312fb7845c0e11ffb4330ce
hbrendan <hbrendan@umich.edu>
2020-05-18 20:51:08 -0500

    final touches

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   loginCallback: '/',
   auth0Namespace: 'https://expoed.co',
-  server: 'https://expoed.appspot.com/api',
-  serverEndpoint: 'https://expoed.appspot.com',
+  server: 'https://isentropic-snow-277701.uc.r.appspot.com/api',
+  serverEndpoint: 'https://isentropic-snow-277701.uc.r.appspot.com',
   stripeClientId: 'ca_H1kNs3sySxclOzNupr8A96iBR756Up2O',
   domain: 'https://expoed.restaurant/#',
   stripeKey: 'pk_live_AMKigygFeFOpLHertDnQ4VMv00ZiCRnG6V'
